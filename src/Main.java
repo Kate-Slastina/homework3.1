d3ddbc1b603d0eda42e1962879e22d70ba340384
Kate-Slastina <114435440+Kate-Slastina@users.noreply.github.com>
2022-10-04 17:16:48 +0300

    number 4

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] bruh = new int[10];
         int n =1_000_000;
-        for (int i =0;i< bruh.length;++i){
+        for (int i =0;i<10;++i){
             int k = in.nextInt();
-            bruh[i]=k;
-            if (bruh[i]>n){
-                System.out.println(bruh[i]);
+            if (k>n){
+                System.out.println(k);
             }
-            n=bruh[i];
+            n=k;
         }
     }
 }
