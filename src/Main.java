5e679682b735766a1fa7cbdc5ecec97fbefd3bc2
Kate-Slastina <114435440+Kate-Slastina@users.noreply.github.com>
2022-10-04 16:52:03 +0300

    number 2

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int m = in.nextInt();
-        int k = in.nextInt();
         int n =0;
         int i =1;
         while (m>0){
-            if (m%10 != k) {
-                n = n + m % 10 * i;
-                i = i * 10;
-            }
+            n = n*10;
+            n = n + m%10;
             m=m/10;
         }
         System.out.println(n);
